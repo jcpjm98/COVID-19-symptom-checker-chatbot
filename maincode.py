bbc0290401f707310efbbe70c01871013e588a1a
nidakt <59667403+nidakt@users.noreply.github.com>
2020-04-24 16:54:49 -0400

    Update maincode.py
    Created base code, which a series of Q&A inputs

--- maincode.py
+++ maincode.py
@@ -1,1 +1,34 @@
+Welcome to the COVID-19 Symptom Checker!
+Please answer the following questions with either "yes" or "no"
 
+travel = input("Have you recently travelled? ")
+print(travel)
+
+if yes
+low risk
+
+work = input ("Are you an essential worker? ")
+print(work)
+
+if yes
+medium risk
+
+contact = input ("Have you been in contact with someone who has either travelled and/or is an essential worker AND is now sick? ")
+print(contact)
+
+if yes
+medium risk
+
+exposure = input ("Have you been in contact with anyone who was known to have the coronavirus or have been exposed to it (COVID-19)? ")
+print(contact)
+
+if yes
+high risk
+
+symptoms = input("Are you experiencing any of the following symptoms: fever, dry cough, sore throat, runny nose ")
+print(fever)
+
+if yes
+high risk
+
+Please contact your medical care provider if you are experiencing symptoms and have been judged to be at medium risk for more than 2 questions
