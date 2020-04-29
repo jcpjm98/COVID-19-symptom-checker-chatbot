e23a01fd9011bdad236225ad24ae73ce027678e2
nidakt <59667403+nidakt@users.noreply.github.com>
2020-04-29 11:17:27 -0400

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,43 +1,48 @@
 Welcome to the COVID-19 Symptom Checker!
 Please answer the following questions with either "yes" or "no"
 
+list = (" ")
+
 travel = input("Have you recently travelled? ")
 print(travel)
 
-if yes
-low risk
+if travel.starswith("y"):
+print("low risk")
 
 work = input ("Are you an essential worker? ")
 print(work)
 
-if yes
-medium risk
+if work.starswith("y"):
+print("medium")
 
 contact = input ("Have you been in contact with someone who has either travelled and/or is an essential worker AND is now sick? ")
 print(contact)
 
-if yes
-medium risk
+if contact.starswith("y"):
+print("medium")
 
 exposure = input ("Have you been in contact with anyone who was known to have the coronavirus or have been exposed to it (COVID-19)? ")
 print(contact)
 
-if yes
-high risk
+if exposure.starswith("y"):
+print("high")
 
 symptoms = input("Are you experiencing any ONE of the following symptoms: fever, cough, sore throat, runny nose, shortness or breath or difficulty breathing, chills, repeated shaking with chills, muscle pain, headache, sore throat, new loss of taste or smell")
 print(fever)
 
-if yes
-low risk
+if symptoms.starswith("y"):
+print("low")
 
-symptoms = input("Are you experiencing TWO OR MORE of the following symptoms: fever, cough, sore throat, runny nose, shortness or breath or difficulty breathing, chills, repeated shaking with chills, muscle pain, headache, sore throat, new loss of taste or smell")
+symptomstwo = input("Are you experiencing TWO OR MORE of the following symptoms: fever, cough, sore throat, runny nose, shortness or breath or difficulty breathing, chills, repeated shaking with chills, muscle pain, headache, sore throat, new loss of taste or smell")
 print(symptoms)
 
-if yes
-high risk
+if symptomstwo.starswith("y"):
+print("high")
 
 symptomshigh = input("If you are experiencing trouble breathing, persistent pain or pressure in the chest, new confusion or inability to arouse, bluish lips or face - please call 911")
 print(symptomshigh)
 
-Please contact your medical care provider if you are experiencing symptoms and have been judged to be at medium risk for more than 2 questions
+if symptoms.starswith("y"):
+print("extremely high")
+
+Please contact your medical care provider if you are experiencing symptoms and have been judged to be at medium or high risk for more than 2 questions
